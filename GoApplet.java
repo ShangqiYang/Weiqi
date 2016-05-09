12cc2c181a12fd56f762a460ebf4ed98ecc7d45d
ProXing <xli89@stevens.edu>
2016-05-08 23:10:55 -0400

    pushpushpush

---
+++ GoApplet.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+public class GoApplet extends Applet{
+    public void init(){
+        new MainWindow().show();
+    }
+}
