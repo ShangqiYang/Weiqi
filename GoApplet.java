ffe87ec69586ce39f53ed75c66954bec228d8fbf
HanxingZhang <zhanghanxing026@gmail.com>
2016-05-08 23:01:31 -0400

    Delete GoApplet.java

--- GoApplet.java
+++
@@ -1,9 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-public class GoApplet extends Applet{
-    public void init(){
-        new MainWindow().show();
-    }
-}
